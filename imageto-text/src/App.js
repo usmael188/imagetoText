da5ad7e0259ab57949b587332fe9ebb24278452c
Usmael Abdurhaman <uabdureman@gmail.com>
2023-11-05 20:47:38 +0000

    error msg

--- imageto-text/src/App.js
+++ imageto-text/src/App.js
@@ -70,26 +70,26 @@ function App() {
                 className='w-full max-w-lg mt-4'
               />
             </div>
+
           )}
 
-          {/* Display Cleaned Text */}
-          <div className='text-center'>
-            <h1 className='text-gray-800 mt-4'>Extracted Text</h1>
-            <textarea
-              className='border-2 p-2 w-full h-64'
-              value={cleanedText}
-              onChange={(e) => setCleanedText(e.target.value)}
-              placeholder='Extracted text will appear here'
-            />
-          </div>
+<div className='flex flex-col items-center mt-8'>
+  {error && (
+    <div className='bg-red-100 text-red-500 p-2 rounded text-center mb-4'>
+      {error}
+    </div>
+  )}
+  <textarea
+    className='border-2 p-2 w-full max-w-lg h-48'
+    value={cleanedText}
+    onChange={(e) => setCleanedText(e.target.value)}
+    placeholder='Extracted text will appear here'
+  />
+</div>
+
         </div>
 
-        {/* Error Message */}
-        {error && (
-          <div className='text-red-500 mt-4 text-center'>
-            {error}
-          </div>
-        )}
+      
       </div>
 
       {/* Footer */}
